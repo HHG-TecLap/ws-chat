a4f05189e6d2239e73b2fc6a4c4525506d115d06
PascalRombach <pasc.rombach@heinrichheinedo.de>
2024-03-08 07:52:50 +0100

    Implemented config and main page redirect

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
-from fileroutes import create_directory_router
+import logging
 
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from fileroutes import create_directory_router
+from config_loader import read_config
+
+
+config = read_config()
+    
 app = FastAPI(
-    routes=create_directory_router("remote_files")
+    title=config["web"]["app_title"],
+    routes=create_directory_router(config["web"]["remote_files_loc"])
 )
+
+@app.get("/")
+def reroute_to_index():
+    return RedirectResponse("/index.html", 301)
 
 
 if __name__ == "__main__":
     import uvicorn
+    logging.warning("Running ASGI Server from interpreter. This is unsafe and should only be used for debugging!")
     uvicorn.run(app)
